cc04fb011cbcb6667519fd17cde3c1b429194b69
Decode <amitkumar696945@gmail.com>
2024-01-06 21:14:07 +0530

    solved errors

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -5,8 +5,9 @@ import './app.css'
   const [todo , setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
 
-  function setTodo{
-
+  function addtodo(){
+    setTodoList([...todoList, todo]) 
+    console.log(todoList)
   }
  
   return (
@@ -15,7 +16,7 @@ import './app.css'
     <div className="container">
         <div className="header">Todo List</div>
         <input type="text" placeholder='Add Todo' value={todo} onChange={(e) =>setTodo(e.target.value)} />
-        <button onClick={setTodo} >Add Todo</button>
+        <button onClick={addtodo} >Add Todo</button>
     </div>
     </>
   )
