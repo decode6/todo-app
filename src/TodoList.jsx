972dbc7efd3f0f1b09dda9c4d7b179361df46794
Decode <amitkumar696945@gmail.com>
2024-01-06 21:05:05 +0530

    activity

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './app.css'
 
-const TodoList = () => {
-   const [todo, setTodo] = useState("")
+ export const TodoList = () => {
+  const [todo , setTodo] = useState("")
+ 
   return (
     
     <>
     <div className="container">
         <div className="header">Todo List</div>
-        <input type="text" placeholder='Add Todo' value={todo} />
-
+        <input type="text" placeholder='Add Todo' value={todo} onChange={(e) =>setTodo(e.target.value)} />
+        <button>Add Todo</button>
     </div>
     </>
   )
